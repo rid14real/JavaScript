fc74299bb0e2b2a4d1b0dde030c1d82f9b76ea80
Ridwan Badmus <62150969+rid14real@users.noreply.github.com>
2021-04-02 23:53:50 +0100

    Create instance_of_an_object.js

---
+++ instance_of_an_object.js
@@ -0,0 +1,17 @@
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+// Only change code below this line
+
+let duck = Object.create(Animal.prototype); // Change this line
+let beagle = Object.create(Animal.prototype); // Change this line
+
+// Alternatively, you can create an instance of an object as follows
+let duck = new Animal();
+let beagle = new Animal();
